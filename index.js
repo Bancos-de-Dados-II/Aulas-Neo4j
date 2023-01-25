dfa578aa5501bbb119030f3dd070eb0cf87bbdfa
Paulo Freitas <paulo.freitas.nt@gmail.com>
2023-01-25 13:44:04 -0300

    Adicionando função para salvar

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ var driver = neo4j.driver(
   'bolt://localhost:7687',
   neo4j.auth.basic('neo4j', 'neo4j123')
 );
+
+async function salvarPessoa(pessoa){
+    var session = driver.session();
+
+    const retorno = await session.run(
+        'CREATE (:Pessoa{nome:$nome, email:$email})',
+        {nome: pessoa.nome,
+        email: pessoa.email});
+
+    console.log(retorno);
+
+    await session.close();
+    await driver.close();
+
+}
